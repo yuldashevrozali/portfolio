bbe4f13a048a575319cd13ab339684f38e77fad8
Yuldashevrozali <Yuldashevrozali08@gmail.com>
2024-06-20 13:38:15 +0500

    dfdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Hero from './components/Hero.jsx'
-// import Header from './components/header/index.jsx'
 import Skills from './components/skills/index.jsx'
 import Projects from './components/projects/index.jsx'
 import Contact from './components/contact/index.jsx'
@@ -10,7 +9,6 @@ function App() {
 
   return (
     <>
-     {/* <Header /> */}
      <BrowserRouter>
      <Routes>
      <Route path='/hero' element = {<Hero />} ></Route>
